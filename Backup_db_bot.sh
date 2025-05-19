ec504a82b5d43d2c7a9987dbe4464f29c1216c9a
Alexandre Jr Miranda <49948786+alejuniormsn@users.noreply.github.com>
2025-05-19 10:09:12 -0300

    Update Backup_db_bot.sh

--- Backup_db_bot.sh
+++ Backup_db_bot.sh
@@ -1,4 +1,4 @@
 echo Iniciando backup do bando de dados... \
-&& PGPASSWORD="Ett@2024" pg_dump -U botdbett -h 192.168.0.27 -p 5432 botdbett > /home/dev/Backup_db_bot/$(date +%Y%m%d-%H%M%S)-botdbett.sql \
+&& PGPASSWORD="PGPASSWORD" pg_dump -U botdb -h 192.168.0.27 -p 5432 botdb > /home/dev/Backup_db_bot/$(date +%Y%m%d-%H%M%S)-botdbett.sql \
 && echo Pronto!
 
